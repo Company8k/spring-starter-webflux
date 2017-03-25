b198a65a9f6fef079e6efb9a3877d40f263acc06
Ante Pavic <nt@eigh.tk>
2017-03-25 21:52:25 +0100

    removed maven wrapper, init

---
+++ src/main/java/tk/eigh/WebflusApplication.java
@@ -0,0 +1,12 @@
+package tk.eigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebflusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebflusApplication.class, args);
+	}
+}
---
+++ src/test/java/tk/eigh/WebflusApplicationTests.java
@@ -0,0 +1,16 @@
+package tk.eigh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebflusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
